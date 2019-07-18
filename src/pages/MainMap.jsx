d570a5ade9ef5cb335bbfa2e7beb790d9e8493c0
Camille Jacques <camille.jacques75@gmail.com>
2019-07-18 18:59:09 +0200

    merge conflicts

--- src/pages/MainMap.jsx
+++ src/pages/MainMap.jsx
@@ -1,29 +1,27 @@
-import React from "react";
-import { compose, withProps } from "recompose";
-import {
-  withScriptjs,
-  withGoogleMap,
-  GoogleMap,
-  Marker
-} from "react-google-maps";
+import React, { Component } from "react";
+import { Map, GoogleApiWrapper } from "google-maps-react";
 
-const MyMapComponent = compose(
-  withProps({
-    googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${
-      process.env.REACT_APP_API_KEY
-    }&v=3.exp&libraries=geometry,drawing,places`,
-    loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `400px` }} />,
-    mapElement: <div style={{ height: `100%` }} />
-  }),
-  withScriptjs,
-  withGoogleMap
-)(props => (
-  <GoogleMap defaultZoom={15} defaultCenter={{ lat: 48.857803, lng: 2.380286 }}>
-    {props.isMarkerShown && (
-      <Marker position={{ lat: 48.857803, lng: 2.380286 }} />
-    )}
-  </GoogleMap>
-));
+const mapStyles = {
+  width: "50%",
+  height: "50%"
+};
 
-export default MyMapComponent;
+export class MapContainer extends Component {
+  render() {
+    return (
+      <Map
+        google={this.props.google}
+        zoom={15}
+        style={mapStyles}
+        initialCenter={{
+          lat: 48.857803,
+          lng: 2.380286
+        }}
+      />
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyBHwOJfv_9R95WIwNJF6jZ6QOrWztObtSo"
+})(MapContainer);
