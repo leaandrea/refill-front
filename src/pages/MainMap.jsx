e24ba3dde150c42b14126540c782d9de0c153e0f
Camille Jacques <camille.jacques75@gmail.com>
2019-07-18 15:44:16 +0200

    map

--- src/pages/MainMap.jsx
+++ src/pages/MainMap.jsx
@@ -9,8 +9,9 @@ import {
 
 const MyMapComponent = compose(
   withProps({
-    googleMapURL:
-      "https://maps.googleapis.com/maps/api/js?key=AIzaSyBHwOJfv_9R95WIwNJF6jZ6QOrWztObtSo&v=3.exp&libraries=geometry,drawing,places",
+    googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${
+      process.env.REACT_APP_API_KEY
+    }&v=3.exp&libraries=geometry,drawing,places`,
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `400px` }} />,
     mapElement: <div style={{ height: `100%` }} />
