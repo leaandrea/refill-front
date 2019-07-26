96bb837559a229303f574d1262f5d2c8e8844c34
Cecile Pham <sheshiru.van@gmail.com>
2019-07-26 16:05:27 +0200

    add gazuse icon

--- src/components/GoogleMap.jsx
+++ src/components/GoogleMap.jsx
@@ -1,21 +1,12 @@
 import React, { Component } from "react";
 import { Map, GoogleApiWrapper, InfoWindow, Marker } from "google-maps-react";
 
-
-
-
-
-
-
-
 export class GoogleMap extends Component {
   state = {
     showingInfoWindow: false,
     activeMarker: {},
     selectedPlace: {}
   };
-
-
 
   onMarkerClick = (props, marker, e) => {
     this.setState({
@@ -34,27 +25,16 @@ export class GoogleMap extends Component {
     }
   };
 
-
-
   render() {
     return (
-
       <Map
-
         google={this.props.google}
         zoom={15}
-
-
-
         initialCenter={{
           lat: this.props.initialCenter.lat,
           lng: this.props.initialCenter.lng
         }}
-
-
-
         gestureHandling="cooperative"
-
       >
         {this.props.markers.map((marker, i) =>
           marker.type === "fontaine" ? (
@@ -65,39 +45,39 @@ export class GoogleMap extends Component {
                 address={marker.address ? marker.address : "Fontaine"}
                 position={{ lat: marker.lat, lng: marker.lng }}
                 icon={{
-                  url: "/images/gazeuse_icon.png",
-                  scaledSize: new this.props.google.maps.Size(14, 14)
+                  url: "/images/gazeuse4.png",
+                  scaledSize: new this.props.google.maps.Size(11.8, 11.8)
                 }}
                 gazeuse={marker.gazeuse}
               />
             ) : (
-                <Marker
-                  key={i}
-                  onClick={this.onMarkerClick}
-                  address={marker.address ? marker.address : "Fontaine"}
-                  position={{ lat: marker.lat, lng: marker.lng }}
-                  icon={{
-                    url: "/images/bluedot_icon.png",
-                    scaledSize: new this.props.google.maps.Size(10, 10)
-                  }}
-                  gazeuse={marker.gazeuse}
-                />
-              )
-          ) : (
               <Marker
                 key={i}
                 onClick={this.onMarkerClick}
                 address={marker.address ? marker.address : "Fontaine"}
                 position={{ lat: marker.lat, lng: marker.lng }}
                 icon={{
-                  url: "/images/commerces.png",
-                  scaledSize: new this.props.google.maps.Size(11.5, 11.5)
+                  url: "/images/bluedot_icon.png",
+                  scaledSize: new this.props.google.maps.Size(10, 10)
                 }}
-                name={marker.name}
                 gazeuse={marker.gazeuse}
-                type={marker.type}
               />
             )
+          ) : (
+            <Marker
+              key={i}
+              onClick={this.onMarkerClick}
+              address={marker.address ? marker.address : "Fontaine"}
+              position={{ lat: marker.lat, lng: marker.lng }}
+              icon={{
+                url: "/images/commerces.png",
+                scaledSize: new this.props.google.maps.Size(11.5, 11.5)
+              }}
+              name={marker.name}
+              gazeuse={marker.gazeuse}
+              type={marker.type}
+            />
+          )
         )}
         <InfoWindow
           marker={this.state.activeMarker}
